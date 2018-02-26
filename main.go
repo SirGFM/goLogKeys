4f7b676fac6125dd6db70977a20e321f08d2334e
SirGFM <gfmandaji@hotmail.com>
2018-02-26 07:53:16 -0300

    Implement the key logger WebSocket server
    The key filter must still be parsed from the input JSON file. There are also a few error which
    are checked if non-nil but ignored... ideally, something should be done with those errors.

--- main.go
+++ main.go
@@ -1,51 +1,108 @@
 package main
 
 import (
-    "github.com/SirGFM/goLogKeys/logger"
-
+    "encoding/json"
     "fmt"
-    "time"
+    "github.com/SirGFM/GoWebSocketProxy/websocket"
+    "github.com/SirGFM/goLogKeys/logger"
+    "github.com/SirGFM/goLogKeys/sender"
+    "io"
+    "os"
 )
 
-func main() {
-    l, err := logger.GetLogger()
+type Args struct {
+    Url string
+    Uri string
+    Port int
+    // NOTE: This field is filled from the information found on the JSON file.
+    acceptedKeys []logger.Key
+}
+
+// Parse the input file as a JSON. The file must not have anything other then a
+// single 'Args' object.
+func parseArgs(filePath string) *Args {
+    var args Args
+
+    f, err := os.Open(filePath)
     if err != nil {
-        panic(err.Error())
+        panic(fmt.Sprintf("Failed to open the input file: %+v", err))
     }
-    defer l.Clean()
+    defer f.Close()
 
-    if err := l.Setup(); err != nil {
-        panic(err.Error())
+    dec := json.NewDecoder(f)
+
+    err = dec.Decode(&args)
+    if err != nil {
+        panic(fmt.Sprintf("Failed to parse the input file: %+v", err))
     }
 
-    running := true
-    go func() {
-        var keys []logger.Key
-        var states []logger.KeyState
+    _, err = dec.Token()
+    if err != io.EOF {
+        panic("Found trailing data at the end of the JSON file")
+    }
 
-        for i := 0; running ; i++ {
-            var err error
+    // TODO Parse the keys into acceptedKeys
+    args.acceptedKeys = []logger.Key{
+        logger.Left,
+        logger.Up,
+        logger.Right,
+        logger.Down,
+        logger.Z,
+        logger.X,
+        logger.A,
+        logger.S,
+    }
 
-            keys, states, err = l.PopMulti(keys, states)
-            if err != nil {
-                fmt.Printf("Error: %+v\n", err)
-            } else {
-                for i := range keys {
-                    k := keys[i]
-                    ks := states[i]
-                    fmt.Printf("%+v: %+v\n", k, ks)
-                }
+    return &args
+}
+
+func main() {
+    // Parse the configurations
+    if len(os.Args) != 2 {
+        panic(fmt.Sprintf("Invalid number of arguments! Usage: %s filter.json", os.Args[0]))
+    }
+    args := parseArgs(os.Args[1])
+
+    // Prepare the key logger
+    keyLogger, err := logger.GetLogger()
+    if err != nil {
+        panic(fmt.Sprintf("Failed to setup the logger: %+v", err))
+    }
+    defer keyLogger.Clean()
+
+    // Create the WebSocket server (but do not start handling it just yet)
+    wsCtx := websocket.NewContext(args.Url, args.Uri, args.Port, 1)
+    defer wsCtx.Close()
+    err = wsCtx.Setup(sender.GetTemplate(keyLogger, args.acceptedKeys))
+    if err != nil {
+        panic(fmt.Sprintf("Failed to setup the WebSocket server: %+v", err))
+    }
+
+    // Start logging keys
+    err = keyLogger.Setup()
+    if err != nil {
+        panic(fmt.Sprintf("Failed to start logging keys: %+v", err))
+    }
+    err = keyLogger.Start()
+    if err != nil {
+        panic(fmt.Sprintf("Failed to start logging keys: %+v", err))
+    }
+
+    // Start the WebSocket server
+    go func (ctx *websocket.Context) {
+        cerr := make(chan error)
+        go ctx.Run(cerr)
+
+        for {
+            err := <-cerr
+            if err == nil {
+                break
             }
 
-            time.Sleep(time.Millisecond * 500)
+            fmt.Printf("Got error from server: %+v\n", err)
         }
-    } ()
+    } (wsCtx)
 
-    if err := l.Start(); err != nil {
-        panic(err.Error())
-    }
-
-    l.Wait()
-    running = false
-    return
+    // Wait until the application is closed
+    keyLogger.Wait()
 }
---
+++ sender/sender.go
@@ -0,0 +1,144 @@
+package sender
+
+import (
+    "github.com/SirGFM/GoWebSocketProxy/websocket"
+    "github.com/SirGFM/goLogKeys/logger"
+    "time"
+)
+
+// How many updates should be sent per second
+const updateRate = 30
+// Delay between each update
+const updateDelay = time.Second / updateRate
+
+type server struct {
+    running bool
+    conn websocket.ClientConnection
+    timer *time.Ticker
+
+    // The main logger, shared between every server instance (and copied from
+    // the main instance).
+    loggerCtx logger.Logger
+    // Filter keys accepted by the sender and map them into their a button bitmask.
+    filter map[logger.Key]uint32
+}
+
+// Retrieve a new sender template, with the shared logger and the map to filter
+// accepted keys.
+func GetTemplate(loggerCtx logger.Logger, acceptedKeys []logger.Key) (
+    websocket.Server) {
+
+    if len(acceptedKeys) == 0 {
+        panic("goLogKeys.sender: Missing keys to be filtered")
+    } else if len(acceptedKeys) > 32 {
+        panic("goLogKeys.sender: Too many keys to be filtered")
+    }
+
+    filter := make(map[logger.Key]uint32)
+    i := uint32(1)
+    for _, key := range acceptedKeys {
+        filter[key] = i
+        i <<= 1
+    }
+
+    return &server{
+        loggerCtx: loggerCtx,
+        filter: filter,
+    }
+}
+
+// Start a new timer for the connection, and the goroutine that shall send the
+// inputs to the client.
+func (s *server) Clone(conn websocket.ClientConnection) (websocket.Server, error) {
+    t := time.NewTicker(updateDelay)
+
+    newServer := server{
+        running: true,
+        conn:    conn,
+        timer:   t,
+        loggerCtx:   s.loggerCtx,
+        filter:   s.filter,
+    }
+
+    // Start sending data
+    go newServer.run()
+
+    return &newServer, nil
+}
+
+// Release resources associated with the timer and stops the goroutine.
+func (srv *server) Cleanup() {
+    srv.running = false
+    time.Sleep(updateDelay * 2)
+    srv.timer.Stop()
+}
+
+// The client should never send any data, so simply ignore it...
+func (*server) Do(msg []byte, offset int) (err error) {
+    return
+}
+
+// Lookup table used to slightly speed up converting an integer value into a
+// string.
+const _itoaLookup = "0123456789ABCDEF"
+
+// Converts a bitmask (store as a uint32) into a byte slice. The value is
+// written in big-endian format, as a hexadecimal number.
+func itoa(buf []byte, mask uint32) (out []byte) {
+    out = buf[:0]
+
+    for ; mask != 0; mask >>= 4 {
+        out = append(out, _itoaLookup[mask & 0xF])
+    }
+
+    return
+}
+
+// Keep sending data to the client.
+func (srv *server) run() {
+    var keys []logger.Key
+    var states []logger.KeyState
+    var tmpBuf []byte
+
+    for {
+        var err error
+
+        state := uint32(0)
+
+        // Wait for a new update
+        _ = <-srv.timer.C
+        if !srv.running {
+            break
+        }
+
+        // Read every key press since the last update
+        for {
+            keys, states, err = srv.loggerCtx.PopMulti(keys, states)
+            if err != nil {
+                // TODO Do something with the error!
+            } else if len(keys) == 0 {
+                break
+            }
+
+            // Filter only the desired keys
+            for i, s := range states {
+                // If the key isn't accepted by the logger (i.e., if it isn't in
+                // the map), the map returns 0.
+                bit := srv.filter[keys[i]]
+
+                if s == logger.Pressed {
+                    state |= bit
+                } else {
+                    state &^= bit
+                }
+            }
+        }
+
+        // Send the packed keys
+        tmpBuf = itoa(tmpBuf, state)
+        _, err = srv.conn.Write(tmpBuf)
+        if err != nil {
+            // TODO Do something with the error!
+        }
+    }
+}
